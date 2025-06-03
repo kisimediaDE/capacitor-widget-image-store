25f497f55389854ffcf646a1cddbccb6f744bda4
Kisimedia.de <kontakt@kisimedia.de>
2025-06-03 10:53:47 +0200

    Bring back android - preperation for implementation

---
+++ android/src/main/java/de/kisimedia/plugins/widgetimagestore/WidgetImageStore.java
@@ -0,0 +1,11 @@
+package de.kisimedia.plugins.widgetimagestore;
+
+import android.util.Log;
+
+public class WidgetImageStore {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
